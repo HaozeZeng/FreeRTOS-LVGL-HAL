7ae0b775608a21b9fc3ba3dbcd57f43a7edb5107
HaozeZeng <1746109233@qq.com>
2024-10-23 11:36:04 +0800

    文件系统Beta版本:初始化就遍历全部文件系统

--- Core/User/app3.c
+++ Core/User/app3.c
@@ -3,12 +3,11 @@
 #include "malloc.h"
 #include "lvgl.h"
 #include "ff.h"
-#include "font_show.h"
-#include "font_update.h"
+
 
 #define scr_get_width() lv_obj_get_width(lv_scr_act())
 #define scr_get_height() lv_obj_get_height(lv_scr_act())
-LV_FONT_DECLARE(yahei16)
+LV_FONT_DECLARE(SanJiBang16)
 
 FRESULT MenuInit(const char *path, lv_obj_t *submenu);
 
@@ -18,32 +17,69 @@ struct MYFILEINFO {
     char *path;
     char *name;
 };
-static lv_style_t label_style;									// ����һ�����
+static lv_style_t font_style;
 
 void test() {
+//    FRESULT result;
+//    FIL file;
+//    char file_content[2048];
+//    char text[100];
+//    UINT bytes_read;
+    lv_style_init(&font_style);
+    lv_style_set_text_font(&font_style, &SanJiBang16);
+//    lv_style_set_text_align(&font_style, LV_TEXT_ALIGN_CENTER);
+
+    /*Create a label with the new style*/
+//    lv_obj_t *label = lv_label_create(lv_screen_active());
+//    lv_obj_add_style(label, &font_style, 0);
+//    lv_label_set_text(label, "你好");
+//    lv_obj_center(label);
+
+
+
+//    result = f_open(&file, "0:/EBOOK/hello.txt", FA_READ);
+//    if (result != FR_OK) {
+//        printf("open error: %d\r\n", result);
+//        return;
+//    }
+//    lv_obj_t *win = lv_win_create(lv_scr_act());
+//    lv_obj_center(win);
+//    lv_obj_set_size(win, scr_get_width(), scr_get_height());
+//    lv_win_add_title(win, "test1");
+//    lv_obj_t *btnexit = lv_win_add_button(win, LV_SYMBOL_CLOSE, 60);
+//    lv_obj_t *cont = lv_win_get_content(win);  /*Content can be aded here*/
+//    lv_obj_t *temp = NULL;  // ȷ�� temp �ڿ�ʼʱΪ NULL
+//    lv_obj_t *label = lv_label_create(cont);  // �ڸ����� cont �д����±�ǩ
+//    lv_label_set_long_mode(label, LV_LABEL_LONG_WRAP);
+//    file_content[0] = '\0';  // ȷ�� file_content �ڶ�ȡǰ��һ�����ַ���
 //
-
-
-
-//    lv_style_init(&label_style);									// ��ʼ�����
-//    lv_style_set_text_font(&label_style, &myFont);// ���÷�������
+//    do {
+//        result = f_read(&file, text, sizeof(text) - 1, &bytes_read);  // ÿ������ȡ 99 ���ֽ�
+//        if (result != FR_OK) {
+//            printf("read error: %d\r\n", result);
+//            break;
+//        }
 //
-//    lv_obj_t * textlabel = lv_label_create(lv_scr_act());			// ������Ļ����һ����ǩ
-//    lv_label_set_long_mode(textlabel,LV_LABEL_LONG_WRAP );				// ��ǩ�����ݿ򣬱��ֿؼ���ȣ����ݹ����ͻ���
-//    lv_obj_set_width(textlabel, 320);									// ���ñ�ǩ���
-//    lv_label_set_recolor(textlabel, true);								// ʹ���ַ��������¶��ַ���ɫ
-//    lv_label_set_text(textlabel, "����SD���ֿ�Ŷ,����SD������123abcABC");	// ������ʾ�ı�
-//    lv_obj_align(textlabel, LV_ALIGN_CENTER, 0, -60);				// ���뵽����ƫ��
-//    lv_obj_add_style(textlabel, &label_style,LV_PART_MAIN);		// Ӧ��Ч�����
-//    //lv_obj_t * List = lv_list_create(lv_scr_act());
+//        text[bytes_read] = '\0';  // ȷ���ַ����� '\0' ��β
+//
+//        // ��� file_content ���Ƿ����㹻�Ŀռ���׷�� text
+//        if ((strlen(file_content) + bytes_read) < 2048) {
+//            strncat(file_content, text, bytes_read);  // ʹ�� strncat ��ֹ���
+//        } else {
+//            printf("Buffer overflow: file content too large.\r\n");
+//            break;
+//        }
+//
+//    } while (bytes_read > 0);
+//    lv_obj_add_style(label, &font_style, 0);
+//    lv_label_set_text(label, file_content);
 
-//    lv_obj_t *lb = lv_label_create(lv_scr_act());
-//    lv_obj_set_style_text_font(lb, &yahei16, 0);
-//    lv_label_set_text(lb, "你");
-//    lv_obj_center(lb);
 
+
+///*使用字体*/
+///*如果不再需要字体，释放字体*/
     char path[30] = "0:";
-  //  lv_style_set_text_font(&label_style, &yahei16);
+    //  lv_style_set_text_font(&label_style, &yahei16);
     MainMenu = lv_list_create(lv_scr_act());
     lv_obj_center(MainMenu);
     lv_obj_set_size(MainMenu, scr_get_width(), scr_get_height());
@@ -56,12 +92,12 @@ void test() {
 static void BtnDirEvent_Cb(lv_event_t *e) {
     lv_event_code_t code = lv_event_get_code(e);
     lv_obj_t *tgt = lv_event_get_target(e);
-    lv_obj_t *submenu = (lv_obj_t *) (e->user_data);
+    lv_obj_t *submenu = (lv_obj_t *) (lv_event_get_user_data(e));
     if (code == LV_EVENT_CLICKED) {
 
         lv_obj_set_style_bg_color(tgt, lv_color_hex(0x47a7f1), LV_STATE_FOCUSED);
         lv_obj_set_style_bg_opa(tgt, 150, LV_STATE_FOCUSED);
-        lv_obj_clear_flag((lv_obj_t *) (e->user_data), LV_OBJ_FLAG_HIDDEN);
+        lv_obj_clear_flag((lv_obj_t *) (lv_event_get_user_data(e)), LV_OBJ_FLAG_HIDDEN);
         //printf("Clicked %s\r\n", lv_list_get_btn_text(submenu,tgt));
     }
 
@@ -70,19 +106,19 @@ static void BtnDirEvent_Cb(lv_event_t *e) {
 static void BtnExitEvent_Cb(lv_event_t *e) {
     lv_event_code_t code = lv_event_get_code(e);
     lv_obj_t *tgt = lv_event_get_target(e);
-    lv_obj_t *submenu = (lv_obj_t *) (e->user_data);
+    lv_obj_t *submenu = (lv_obj_t *) (lv_event_get_user_data(e));
     if (code == LV_EVENT_CLICKED) {
 
         lv_obj_set_style_bg_color(tgt, lv_color_hex(0x47a7f1), LV_STATE_FOCUSED);
         lv_obj_set_style_bg_opa(tgt, 150, LV_STATE_FOCUSED);
-        lv_obj_add_flag((lv_obj_t *) (e->user_data), LV_OBJ_FLAG_HIDDEN);
+        lv_obj_add_flag((lv_obj_t *) (lv_event_get_user_data(e)), LV_OBJ_FLAG_HIDDEN);
         //printf("Clicked %s\r\n", lv_list_get_btn_text(submenu,tgt));
     }
 
 }
 
 static void BtnFileEvent_Cb(lv_event_t *e) {
-    struct MYFILEINFO *info = (struct MYFILEINFO *) (e->user_data);
+    struct MYFILEINFO *info = (struct MYFILEINFO *) (lv_event_get_user_data(e));
     lv_event_code_t code = lv_event_get_code(e);
     lv_obj_t *tgt = lv_event_get_target(e);
     FIL file;
@@ -101,11 +137,13 @@ static void BtnFileEvent_Cb(lv_event_t *e) {
             printf("open error: %d\r\n", result);
             return;
         }
-        lv_obj_t *win = lv_win_create(lv_scr_act(), 40);
+        lv_obj_t *win = lv_win_create(lv_scr_act());
         lv_obj_center(win);
         lv_obj_set_size(win, scr_get_width(), scr_get_height());
-        lv_win_add_title(win, info->name);
-        lv_obj_t *btnexit = lv_win_add_btn(win, LV_SYMBOL_CLOSE, 60);
+        lv_obj_t *title =lv_win_add_title(win, info->name);
+        lv_obj_add_style(title, &font_style, 0);
+
+        lv_obj_t *btnexit = lv_win_add_button(win, LV_SYMBOL_CLOSE, 60);
         lv_obj_add_event_cb(btnexit, BtnExitEvent_Cb, LV_EVENT_CLICKED, win);
         lv_obj_t *cont = lv_win_get_content(win);  /*Content can be aded here*/
 
@@ -133,6 +171,7 @@ static void BtnFileEvent_Cb(lv_event_t *e) {
             }
 
         } while (bytes_read > 0);
+        lv_obj_add_style(label, &font_style, 0);
         lv_label_set_text(label, file_content);
         printf("file_content is %s\r\n", file_content);
 
@@ -161,7 +200,12 @@ FRESULT MenuInit(const char *path, lv_obj_t *submenu) {
         } else {
             snprintf(DirPath, sizeof(DirPath), "%s..", path);
         }
-        lv_obj_t *exitbtn = lv_list_add_btn(submenu, LV_SYMBOL_DIRECTORY, DirPath);
+        lv_obj_t *exitbtn = lv_list_add_btn(submenu, LV_SYMBOL_DIRECTORY, NULL);
+        lv_obj_t *label = lv_label_create(exitbtn);
+        lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR);
+//                lv_obj_set_style_text_font(label, &yahei16, 0);
+        lv_label_set_text(label, DirPath);
+        lv_obj_add_style(label, &font_style, 0);
         lv_obj_add_event_cb(exitbtn, BtnExitEvent_Cb, LV_EVENT_CLICKED, submenu);
 
         while (1) {
@@ -175,12 +219,12 @@ FRESULT MenuInit(const char *path, lv_obj_t *submenu) {
                 printf(" path:%s, File:%s\r\n", path, fno.fname);
                 snprintf(Filepath, sizeof(Filepath), "%s/%s", path, fno.fname);
                 printf(" DirPath:%s\r\n", Filepath);
-                lv_obj_t *btn = lv_list_add_btn(submenu, LV_SYMBOL_DIRECTORY, fno.fname);
-
-//                lv_obj_t * label = lv_label_create(btn);
-//                lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR);
+                lv_obj_t *btn = lv_list_add_btn(submenu, LV_SYMBOL_DIRECTORY, NULL);
+                lv_obj_t *label = lv_label_create(btn);
+                lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR);
 //                lv_obj_set_style_text_font(label, &yahei16, 0);
-//                lv_label_set_text(label, fno.fname);
+                lv_label_set_text(label, fno.fname);
+                lv_obj_add_style(label, &font_style, 0);
 
                 lv_obj_t *SubMenu = lv_list_create(lv_scr_act());
                 lv_obj_center(SubMenu);
@@ -197,8 +241,12 @@ FRESULT MenuInit(const char *path, lv_obj_t *submenu) {
                 snprintf(Filepath, sizeof(Filepath), "%s/%s", path, fno.fname);
                 printf(" File:%s/%s\r\n", path, fno.fname);
                 printf("Filepath is %s\r\n", Filepath);
-                lv_obj_t *btn = lv_list_add_btn(submenu, LV_SYMBOL_FILE, fno.fname);
-
+                lv_obj_t *btn = lv_list_add_btn(submenu, LV_SYMBOL_FILE, NULL);
+                lv_obj_t *label = lv_label_create(btn);
+                lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR);
+//                lv_obj_set_style_text_font(label, &yahei16, 0);
+                lv_label_set_text(label, fno.fname);
+                lv_obj_add_style(label, &font_style, 0);
 //                lv_obj_t * label = lv_label_create(btn);
 //                lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR);
 //                lv_obj_set_style_text_font(label, &yahei16, 0);
